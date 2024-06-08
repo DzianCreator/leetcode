7d43cd818c39c8901e64578005e0d65b46d7c15b
DzianCreator <dziancreator@gmail.com>
2024-06-08 12:03:23 +0200

    Add 'longest substring unique characters'

---
+++ algorithms/Algorithms/LongestSubstringWithoutRepeatingCharacters.cs
@@ -0,0 +1,39 @@
+﻿
+namespace Algorithms
+{
+	public class LongestSubstringWithoutRepeatingCharacters
+	{
+		public static int Calculate(string s)
+		{
+			if (string.IsNullOrEmpty(s))
+			{
+				return 0;
+			}
+			int maxSubLength = 0;
+			int subFirstIndex = 0;
+			Dictionary<char, int> indices = [];
+			for (int i = 0; i < s.Length; ++i)
+			{
+				if (!indices.ContainsKey(s[i]))
+				{
+					indices.Add(s[i], i);
+				}
+				else
+				{
+					int newFirstIndex = indices[s[i]] + 1;
+					for (int j = subFirstIndex; j < newFirstIndex; ++j)
+					{
+						indices.Remove(s[j]);
+					}
+					indices.Add(s[i], i);
+					subFirstIndex = newFirstIndex;
+				}
+				if (indices.Count > maxSubLength)
+				{
+					maxSubLength = indices.Count;
+				}
+			}
+			return maxSubLength;
+		}
+	}
+}
---
+++ algorithms/AlgorithmsTests/LongestSubstringWithoutRepeatingCharactersTests.cs
@@ -0,0 +1,32 @@
+﻿using Algorithms;
+
+namespace AlgorithmsTests
+{
+	public class LongestSubstringWithoutRepeatingCharactersTests
+	{
+		[Theory]
+		[InlineData((string)null)]
+		[InlineData("")]
+		public void NullOrEmpty_ReturnZero(string s)
+		{
+			var result = LongestSubstringWithoutRepeatingCharacters.Calculate(s);
+			Assert.Equal(0, result);
+		}
+
+		[Theory]
+		[InlineData("abcabcbb", 3)]
+		[InlineData("bbbbb", 1)]
+		[InlineData("pwwkew", 3)]
+		[InlineData("aab", 2)]
+		[InlineData("abcbkklmmklmr", 4)]
+		[InlineData("abcdeffytcccyadaysty", 6)]
+		[InlineData("abababab", 2)]
+		[InlineData("abcabcabcabc", 3)]
+		[InlineData("ababababbabab", 2)]
+		public void String_ReturnLength(string s, int expected)
+		{
+			var result = LongestSubstringWithoutRepeatingCharacters.Calculate(s);
+			Assert.Equal(expected, result);
+		}
+	}
+}
