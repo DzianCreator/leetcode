a42a7e110c39a531ea17b5e5ccdd323b39a53d38
DzianCreator <dziancreator@gmail.com>
2024-06-10 17:31:50 +0200

    Add 'zigzag string conversion'

---
+++ algorithms/Algorithms/ZigzagStringConversion.cs
@@ -0,0 +1,58 @@
+﻿
+namespace Algorithms
+{
+	public class ZigzagStringConversion
+	{
+		public static string Calculate(string s, int numRows)
+		{
+			if (s == null)
+			{
+				return string.Empty;
+			}
+			if (numRows < 2)
+			{
+				return s;
+			}
+			int lastRowIndex = numRows - 1;
+			char[] resultArray = new char[s.Length];
+			int resultIndex = 0;
+			int period = numRows + numRows - 2;
+			for (int i = 0; i < s.Length; i += period)
+			{
+				resultArray[resultIndex] = s[i];
+				resultIndex++;
+			}
+			int decreasingPeriod = period;
+			int increasingPeriod = 0;
+			int rowIndex = 1;
+			while (rowIndex < lastRowIndex)
+			{
+				decreasingPeriod -= 2;
+				increasingPeriod += 2;
+				bool isDecreasing = true;
+				int j = rowIndex;
+				while (j < s.Length)
+				{
+					resultArray[resultIndex] = s[j];
+					resultIndex++;
+					if (isDecreasing)
+					{
+						j += decreasingPeriod;
+					}
+					else
+					{
+						j += increasingPeriod;
+					}
+					isDecreasing = !isDecreasing;
+				}
+				rowIndex++;
+			}
+			for (int i = lastRowIndex; i < s.Length; i += period)
+			{
+				resultArray[resultIndex] = s[i];
+				resultIndex++;
+			}
+			return new string(resultArray);
+		}
+	}
+}
---
+++ algorithms/AlgorithmsTests/ZigzagStringConversionTests.cs
@@ -0,0 +1,29 @@
+﻿using Algorithms;
+namespace AlgorithmsTests
+{
+	public class ZigzagStringConversionTests
+	{
+		[Fact]
+		public void NullString_ReturnEmptyString()
+		{
+			string result = ZigzagStringConversion.Calculate(null, 1);
+			Assert.Equal(string.Empty, result);
+		}
+
+		[Theory]
+		[InlineData("PAYPALISHIRING", 3, "PAHNAPLSIIGYIR")]
+		[InlineData("PAYPALISHIRING", 4, "PINALSIGYAHRPI")]
+		[InlineData("babad", 2, "bbdaa")]
+		[InlineData("acbrbca", 1, "acbrbca")]
+		[InlineData("a", 2, "a")]
+		[InlineData("ptrtt", 5, "ptrtt")]
+		[InlineData("ptrtt", 10, "ptrtt")]
+		[InlineData("ptrtt", 4, "ptrtt")]
+		[InlineData("abcde", 4, "abced")]
+		public void String_ReturnConverted(string input, int numRows, string expected)
+		{
+			string result = ZigzagStringConversion.Calculate(input, numRows);
+			Assert.Equal(expected, result);
+		}
+	}
+}
