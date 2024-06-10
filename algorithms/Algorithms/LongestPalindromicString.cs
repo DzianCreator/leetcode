230c5a112a48c2baf72ec80acce55cf72dbd3853
DzianCreator <dziancreator@gmail.com>
2024-06-10 12:58:37 +0200

    Add "longest palindromic string"

---
+++ algorithms/Algorithms/LongestPalindromicString.cs
@@ -0,0 +1,60 @@
+﻿
+namespace Algorithms
+{
+	public class LongestPalindromicString
+	{
+		public static string Calculate(string s)
+		{
+			if (string.IsNullOrEmpty(s))
+			{
+				return string.Empty;
+			}
+			int i = 0;
+			int iStart = 0;
+			int pLength = 0;
+			while (i < s.Length)
+			{
+				int lower = i - 1;
+				int higher = i + 1;
+				int curLength = 1;
+				//odd
+				while (lower > -1 && higher < s.Length)
+				{
+					if (s[lower] == s[higher])
+					{
+						lower--;
+						higher++;
+						curLength += 2;
+					}
+					else break;
+				}
+				if (curLength > pLength)
+				{
+					iStart = lower + 1;
+					pLength = curLength;
+				}
+				//even
+				lower = i - 1;
+				higher = i;
+				curLength = 0;
+				while (lower > -1 && higher < s.Length)
+				{
+					if (s[lower] == s[higher])
+					{
+						lower--;
+						higher++;
+						curLength += 2;
+					}
+					else break;
+				}
+				if (curLength > pLength)
+				{
+					iStart = lower + 1;
+					pLength = curLength;
+				}
+				i++;
+			}
+			return s.Substring(iStart, pLength);
+		}
+	}
+}
---
+++ algorithms/AlgorithmsTests/LongestPalindromicStringTests.cs
@@ -0,0 +1,40 @@
+﻿using Algorithms;
+
+namespace AlgorithmsTests
+{
+	public class LongestPalindromicStringTests
+	{
+		[Fact]
+		public void NullString_ReturnEmptyString()
+		{
+			string result = LongestPalindromicString.Calculate(null);
+			Assert.Equal(string.Empty, result);
+		}
+
+		[Theory]
+		[InlineData("babad", "bab")]
+		[InlineData("acbrbca", "acbrbca")]
+		[InlineData("a", "a")]
+		[InlineData("", "")]
+		[InlineData("ptrttrtabcecbatattatc", "tabcecbat")]
+		[InlineData("abcdcb", "bcdcb")]
+		[InlineData("bcdcba", "bcdcb")]
+		public void StringWithLongestOddPalindrome_ReturnPalindrome(string input, string expected)
+		{
+			string result = LongestPalindromicString.Calculate(input);
+			Assert.Equal(expected, result);
+		}
+
+		[Theory]
+		[InlineData("bb", "bb")]
+		[InlineData("abbc", "bb")]
+		[InlineData("abb", "bb")]
+		[InlineData("bbbba", "bbbb")]
+		[InlineData("adtcbbctdtatdtatyuo", "dtcbbctd")]
+		public void StringWithLongestEvenPalindrome_ReturnPalindrome(string input, string expected)
+		{
+			string result = LongestPalindromicString.Calculate(input);
+			Assert.Equal(expected, result);
+		}
+	}
+}
