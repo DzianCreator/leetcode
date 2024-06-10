fa481861d3585bf872fec46cab1eae49aada7935
DzianCreator <dziancreator@gmail.com>
2024-06-10 23:01:00 +0200

    Add 'palindrome number'

---
+++ algorithms/Algorithms/IntegerIsPalindrome.cs
@@ -0,0 +1,32 @@
+﻿
+namespace Algorithms
+{
+	public class IntegerIsPalindrome
+	{
+		public static bool Calculate(int x)
+		{
+			if (x < 0)
+			{
+				return false;
+			}
+			if (x < 10)
+			{
+				return true;
+			}
+			string xText = x.ToString();
+			int median = xText.Length / 2;
+			int lowerIndex = median - 1;
+			int higherIndex = (xText.Length % 2 == 1) ? median + 1 : median;
+			while (higherIndex < xText.Length)
+			{
+				if (xText[lowerIndex] != xText[higherIndex])
+				{
+					return false;
+				}
+				higherIndex++;
+				lowerIndex--;
+			}
+			return true;
+		}
+	}
+}
---
+++ algorithms/AlgorithmsTests/IntegerIsPalindromeTests.cs
@@ -0,0 +1,42 @@
+﻿using Algorithms;
+
+namespace AlgorithmsTests
+{
+	public class IntegerIsPalindromeTests
+	{
+		[Theory]
+		[InlineData(0)]
+		[InlineData(1)]
+		[InlineData(5)]
+		[InlineData(9)]
+		public void OneDigitPalindrome_ReturnTrue(int x)
+		{
+			bool result = IntegerIsPalindrome.Calculate(x);
+			Assert.True(result);
+		}
+
+		[Theory]
+		[InlineData(11)]
+		[InlineData(212)]
+		[InlineData(923329)]
+		[InlineData(92329)]
+		public void Palindrome_ReturnTrue(int x)
+		{
+			bool result = IntegerIsPalindrome.Calculate(x);
+			Assert.True(result);
+		}
+
+		[Theory]
+		[InlineData(-1)]
+		[InlineData(-11)]
+		[InlineData(-121)]
+		[InlineData(1232)]
+		[InlineData(12)]
+		[InlineData(9123)]
+		public void NotPalindrome_ReturnFalse(int x)
+		{
+			bool result = IntegerIsPalindrome.Calculate(x);
+			Assert.False(result);
+		}
+	}
+}
