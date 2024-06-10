746565ac4453a659e3100e1a0eb55c34220e5bda
DzianCreator <dziancreator@gmail.com>
2024-06-10 21:45:51 +0200

    Add 'string to integer (atoi)'

---
+++ algorithms/Algorithms/StringToIntegerAtoi.cs
@@ -0,0 +1,64 @@
+﻿
+namespace Algorithms
+{
+	public class StringToIntegerAtoi
+	{
+		public static int Calculate(string s)
+		{
+			if (s == null)
+			{
+				return 0;
+			}
+			int i = 0;
+			while (i < s.Length)
+			{
+				if (s[i] == ' ')
+				{
+					i++;
+				}
+				else break;
+			}
+			if (i == s.Length)
+			{
+				return 0;
+			}
+			bool isNegative = false;
+			if (s[i] == '-' || s[i] == '+')
+			{
+				isNegative = s[i] == '-';
+				i++;
+			}
+			if (i == s.Length)
+			{
+				return 0;
+			}
+			while (i < s.Length && s[i] == '0')
+			{
+				i++;
+			}
+			if (i == s.Length || s[i] < '0' || s[i] > '9')
+			{
+				return 0;
+			}
+			int startIndex = i;
+			while (i < s.Length && s[i] >= '0' && s[i] <= '9')
+			{
+				i++;
+			}
+			int length = i - startIndex;
+			string numberAsText = (isNegative ? "-" : "") + s.Substring(startIndex, length);
+			if (Int32.TryParse(numberAsText, out int number))
+			{
+				return number;
+			}
+			else if (isNegative)
+			{
+				return Int32.MinValue;
+			}
+			else
+			{
+				return Int32.MaxValue;
+			}
+		}
+	}
+}
---
+++ algorithms/AlgorithmsTests/StringToIntegerAtoiTests.cs
@@ -0,0 +1,98 @@
+﻿using Algorithms;
+
+namespace AlgorithmsTests
+{
+	public class StringToIntegerAtoiTests
+	{
+		[Fact]
+		public void NullString_Return0()
+		{
+			int result = StringToIntegerAtoi.Calculate(null);
+			Assert.Equal(0, result);
+		}
+
+		[Fact]
+		public void EmptyString_Return0()
+		{
+			int result = StringToIntegerAtoi.Calculate(string.Empty);
+			Assert.Equal(0, result);
+		}
+
+		[Theory]
+		[InlineData("42", 42)]
+		[InlineData("54321", 54321)]
+		[InlineData("912", 912)]
+		[InlineData("-6432", -6432)]
+		public void NumberString_ReturnNumber(string s, int expected)
+		{
+			int result = StringToIntegerAtoi.Calculate(s);
+			Assert.Equal(expected, result);
+		}
+
+		[Theory]
+		[InlineData("-0", 0)]
+		[InlineData("0", 0)]
+		[InlineData("-00042", -42)]
+		[InlineData("000230", 230)]
+		public void LeadingZero_ReturnNumber(string s, int expected)
+		{
+			int result = StringToIntegerAtoi.Calculate(s);
+			Assert.Equal(expected, result);
+		}
+
+		[Theory]
+		[InlineData("-0asdf", 0)]
+		[InlineData("0fdsa", 0)]
+		[InlineData("-42d", -42)]
+		[InlineData("000230asd", 230)]
+		public void StringEndsWithNonDigits_ReturnNumber(string s, int expected)
+		{
+			int result = StringToIntegerAtoi.Calculate(s);
+			Assert.Equal(expected, result);
+		}
+
+		[Theory]
+		[InlineData("-0-1", 0)]
+		[InlineData("0-1", 0)]
+		[InlineData("-42d234", -42)]
+		[InlineData("000230-3", 230)]
+		public void StringWithManyDigits_ReturnNumber(string s, int expected)
+		{
+			int result = StringToIntegerAtoi.Calculate(s);
+			Assert.Equal(expected, result);
+		}
+
+		[Theory]
+		[InlineData("-d0-1", 0)]
+		[InlineData("tre0-1", 0)]
+		[InlineData("-fdsa42d234", 0)]
+		[InlineData("d000230-3", 0)]
+		public void StringStartsWithNonDigits_ReturnNumber(string s, int expected)
+		{
+			int result = StringToIntegerAtoi.Calculate(s);
+			Assert.Equal(expected, result);
+		}
+
+		[Theory]
+		[InlineData("-", 0)]
+		[InlineData("--", 0)]
+		[InlineData("-+", 0)]
+		[InlineData("+", 0)]
+		public void IncompliteNumber_ReturnNumber(string s, int expected)
+		{
+			int result = StringToIntegerAtoi.Calculate(s);
+			Assert.Equal(expected, result);
+		}
+
+		[Theory]
+		[InlineData("2222222222222222", Int32.MaxValue)]
+		[InlineData("-23232323232323232323", Int32.MinValue)]
+		[InlineData("2147483648", Int32.MaxValue)]
+		[InlineData("-2147483649", Int32.MinValue)]
+		public void OverflowingNumber_ReturnNumber(string s, int expected)
+		{
+			int result = StringToIntegerAtoi.Calculate(s);
+			Assert.Equal(expected, result);
+		}
+	}
+}
