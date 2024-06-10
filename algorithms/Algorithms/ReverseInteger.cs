b81c7063497366503229dabb24c32564e4fd1d67
DzianCreator <dziancreator@gmail.com>
2024-06-10 17:31:59 +0200

    Add 'reverse integer'

---
+++ algorithms/Algorithms/ReverseInteger.cs
@@ -0,0 +1,29 @@
+﻿
+namespace Algorithms
+{
+	public class ReverseInteger
+	{
+		public static int Calculate(int x)
+		{
+			string numberAsText = x.ToString();
+			if (x < 0)
+			{
+				numberAsText = numberAsText.Substring(1);
+			}
+			string reverseNumberAsText = new string(numberAsText.Reverse().ToArray());
+			if (x < 0)
+			{
+				reverseNumberAsText = "-" + reverseNumberAsText;
+			}
+			Int32 result;
+			if (Int32.TryParse(reverseNumberAsText, out result))
+			{
+				return result;
+			}
+			else
+			{
+				return 0;
+			}
+		}
+	}
+}
---
+++ algorithms/AlgorithmsTests/ReverseIntegerTests.cs
@@ -0,0 +1,40 @@
+﻿using Algorithms;
+
+namespace AlgorithmsTests
+{
+	public class ReverseIntegerTests
+	{
+		[Theory]
+		[InlineData(0)]
+		[InlineData(1)]
+		[InlineData(5)]
+		[InlineData(9)]
+		public void OneDigit_ReturnReverse(int x)
+		{
+			var result = ReverseInteger.Calculate(x);
+			Assert.Equal(x, result);
+		}
+
+		[Theory]
+		[InlineData(10, 1)]
+		[InlineData(1000, 1)]
+		[InlineData(1234, 4321)]
+		[InlineData(1221, 1221)]
+		[InlineData(-1000, -1)]
+		[InlineData(-1234, -4321)]
+		public void SmallNumber_ReturnReverse(int x, int expected)
+		{
+			var result = ReverseInteger.Calculate(x);
+			Assert.Equal(expected, result);
+		}
+
+		[Theory]
+		[InlineData(Int32.MaxValue, 0)]
+		[InlineData(Int32.MinValue, 0)]
+		public void OverflowingNumber_ReturnReverse(int x, int expected)
+		{
+			var result = ReverseInteger.Calculate(x);
+			Assert.Equal(expected, result);
+		}
+	}
+}
