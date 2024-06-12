0e06e8436f0b16869c593bad4d90ec7793a0b417
DzianCreator <dziancreator@gmail.com>
2024-06-12 20:12:18 +0200

    Add 'longest valid parentheses

---
+++ algorithms/Algorithms/LongestValidParentheses.cs
@@ -0,0 +1,63 @@
+﻿
+namespace Algorithms
+{
+	public class LongestValidParentheses
+	{
+		public static int Calculate(string s)
+		{
+			if (s == null)
+			{
+				return 0;
+			}
+			bool[] match = new bool[s.Length];
+			int sequenceBreakIndex = -1;
+			for (int i = 0; i < s.Length; ++i)
+			{
+				if (s[i] == '(')
+				{
+					match[i] = false;
+				}
+				else
+				{
+					int j = i - 1;
+					while (j > sequenceBreakIndex)
+					{
+						if (!match[j])
+						{
+							match[i] = true;
+							match[j] = true;
+							break;
+						}
+						j--;
+					}
+					if (j == sequenceBreakIndex)
+					{
+						sequenceBreakIndex = i;
+					}
+				}
+			}
+			int longest = 0;
+			int candidate = 0;
+			for (int i = 0; i < match.Length; ++i)
+			{
+				if (match[i])
+				{
+					candidate++;
+				}
+				else
+				{
+					if (longest < candidate)
+					{
+						longest = candidate;
+					}
+					candidate = 0;
+				}
+			}
+			if (longest < candidate)
+			{
+				longest = candidate;
+			}
+			return longest;
+		}
+	}
+}
---
+++ algorithms/AlgorithmsTests/LongestValidParenthesesTests.cs
@@ -0,0 +1,57 @@
+﻿using Algorithms;
+
+namespace AlgorithmsTests
+{
+	public class LongestValidParenthesesTests
+	{
+		[Fact]
+		public void EmptyString_Return0()
+		{
+			var result = LongestValidParentheses.Calculate("");
+			Assert.Equal(0, result);
+		}
+
+		[Theory]
+		[InlineData("(")]
+		[InlineData(")")]
+		[InlineData(")(")]
+		[InlineData("))(((")]
+		public void InvalidString_Return0(string s)
+		{
+			var result = LongestValidParentheses.Calculate(s);
+			Assert.Equal(0, result);
+		}
+
+		[Theory]
+		[InlineData("(()", 2)]
+		[InlineData(")()", 2)]
+		[InlineData("())", 2)]
+		[InlineData("()(", 2)]
+		[InlineData(")()())", 4)]
+		[InlineData("()(()", 2)]
+		[InlineData("())()", 2)]
+		[InlineData("()((())", 4)]
+		[InlineData("())(())", 4)]
+		[InlineData("((())())))", 8)]
+		[InlineData("()(())())", 8)]
+		[InlineData("(()(())()", 8)]
+		[InlineData("(()))(()()())", 8)]
+		[InlineData("(()(((()", 2)]
+		public void PartlyValid_ReturnLength(string s, int expected)
+		{
+			var result = LongestValidParentheses.Calculate(s);
+			Assert.Equal(expected, result);
+		}
+
+		[Theory]
+		[InlineData("()(())()", 8)]
+		[InlineData("((()))", 6)]
+		[InlineData("((())())", 8)]
+		[InlineData("((()()())()()(()))()", 20)]
+		public void FullyValid_ReturnLength(string s, int expected)
+		{
+			var result = LongestValidParentheses.Calculate(s);
+			Assert.Equal(expected, result);
+		}
+	}
+}
