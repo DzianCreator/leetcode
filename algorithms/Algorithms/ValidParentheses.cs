82e3ee424a0812c205fdaf57b918428a78778b5f
DzianCreator <dziancreator@gmail.com>
2024-06-13 15:55:11 +0200

    Add 'valid parentheses'

---
+++ algorithms/Algorithms/ValidParentheses.cs
@@ -0,0 +1,54 @@
+﻿
+namespace Algorithms
+{
+	public class ValidParentheses
+	{
+		public static bool Calculate(string s)
+		{
+			if (string.IsNullOrEmpty(s))
+			{
+				return false;
+			}
+			Stack<char> validator = new Stack<char>();
+			for (int i = 0; i < s.Length; ++i)
+			{
+				if (s[i] == '{' || s[i] == '(' || s[i] == '[')
+				{
+					validator.Push(s[i]);
+				}
+				else
+				{
+					if (validator.TryPop(out char last))
+					{
+						if (s[i] == '}')
+						{
+							if (last != '{')
+							{
+								return false;
+							}
+						}
+						else if (s[i] == ']')
+						{
+							if (last != '[')
+							{
+								return false;
+							}
+						}
+						else
+						{
+							if (last != '(')
+							{
+								return false;
+							}
+						}
+					}
+					else
+					{
+						return false;
+					}
+				}
+			}
+			return validator.Count == 0;
+		}
+	}
+}
---
+++ algorithms/AlgorithmsTests/ValidParenthesesTests.cs
@@ -0,0 +1,75 @@
+﻿using Algorithms;
+
+namespace AlgorithmsTests
+{
+	public class ValidParenthesesTests
+	{
+		[Fact]
+		public void EmptyString_Return0()
+		{
+			var result = ValidParentheses.Calculate("");
+			Assert.False(result);
+		}
+
+		[Theory]
+		[InlineData("(")]
+		[InlineData(")")]
+		[InlineData(")(")]
+		[InlineData("))(((")]
+		[InlineData("(}")]
+		[InlineData("(]")]
+		[InlineData("{]")]
+		[InlineData("{)")]
+		[InlineData("[)")]
+		[InlineData("[}")]
+		public void InvalidString_ReturnFalse(string s)
+		{
+			var result = ValidParentheses.Calculate(s);
+			Assert.False(result);
+		}
+
+		[Theory]
+		[InlineData("(()")]
+		[InlineData(")()")]
+		[InlineData("())")]
+		[InlineData("()(")]
+		[InlineData(")()())")]
+		[InlineData("()(()")]
+		[InlineData("())()")]
+		[InlineData("()((())")]
+		[InlineData("())(())")]
+		[InlineData("((())())))")]
+		[InlineData("()(())())")]
+		[InlineData("(()(())()")]
+		[InlineData("(()))(()()())")]
+		[InlineData("(()(((()")]
+		[InlineData("()(()){)")]
+		[InlineData("((())]")]
+		[InlineData("(([))())")]
+		[InlineData("((()()())[)()(()))()")]
+		public void PartlyValid_ReturnFalse(string s)
+		{
+			var result = ValidParentheses.Calculate(s);
+			Assert.False(result);
+		}
+
+		[Theory]
+		[InlineData("()(())()")]
+		[InlineData("((()))")]
+		[InlineData("((())())")]
+		[InlineData("((()()())()()(()))()")]
+		[InlineData("()([()])()")]
+		[InlineData("(({({})}))[]")]
+		[InlineData("{((())([]))}")]
+		[InlineData("((()([])([]))([])()((){})){}()")]
+		[InlineData("()(({}))([])")]
+		[InlineData("[[]]{}(){}")]
+		[InlineData("{{}}{}[]((){})")]
+		[InlineData("{[(([](){})[]{}({}))()][{}][()]}")]
+		public void FullyValid_ReturnLength(string s)
+		{
+			var result = ValidParentheses.Calculate(s);
+			Assert.True(result);
+		}
+	}
+}
