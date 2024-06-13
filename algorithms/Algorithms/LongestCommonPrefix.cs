7e8eb28710c03c33574cbdea8548a559bdff4814
DzianCreator <dziancreator@gmail.com>
2024-06-13 14:15:26 +0200

    Add 'longest common prefix'

---
+++ algorithms/Algorithms/LongestCommonPrefix.cs
@@ -0,0 +1,28 @@
+﻿
+namespace Algorithms
+{
+	public class LongestCommonPrefix
+	{
+		public static string Calculate(string[] strs)
+		{
+			if (strs == null || strs.Length == 0)
+			{
+				return "";
+			}
+			int prefixLength = strs[0].Length;
+			for (int i = 1; i < strs.Length; ++i)
+			{
+				int k = 0;
+				while (k < prefixLength && k < strs[0].Length && k < strs[i].Length && strs[0][k] == strs[i][k])
+				{
+					k++;
+				}
+				if (k < prefixLength)
+				{
+					prefixLength = k;
+				}
+			}
+			return strs[0].Substring(0, prefixLength);
+		}
+	}
+}
---
+++ algorithms/AlgorithmsTests/LongestCommonPrefixTests.cs
@@ -0,0 +1,70 @@
+﻿using Algorithms;
+
+namespace AlgorithmsTests
+{
+	public class LongestCommonPrefixTests
+	{
+		[Fact]
+		public void EmptyArray_ReturnEmptyString()
+		{
+			string result = LongestCommonPrefix.Calculate(new string[0]);
+			Assert.Equal("", result);
+		}
+
+		[Fact]
+		public void AllStringsWithPrefix1_ReturnPrefix()
+		{
+			string[] strs = { "flower", "flow", "flight" };
+			string result = LongestCommonPrefix.Calculate(strs);
+			Assert.Equal("fl", result);
+		}
+
+		[Fact]
+		public void AllStringsWithPrefix2_ReturnPrefix()
+		{
+			string[] strs = { "abcdef", "abcd", "abcdefjh", "abcdefj", "abcdklm" };
+			string result = LongestCommonPrefix.Calculate(strs);
+			Assert.Equal("abcd", result);
+		}
+
+		[Fact]
+		public void AllStringsWithPrefix3_ReturnPrefix()
+		{
+			string[] strs = { "abcde", "abcdfg", "abcdop", "abcdrml", "abcdfgh", "abcderml", "abcdrmlef" };
+			string result = LongestCommonPrefix.Calculate(strs);
+			Assert.Equal("abcd", result);
+		}
+
+		[Fact]
+		public void OnString_ReturnString()
+		{
+			string[] strs = { "abcd" };
+			string result = LongestCommonPrefix.Calculate(strs);
+			Assert.Equal("abcd", result);
+		}
+
+		[Fact]
+		public void SomeStringsWithPrefix_ReturnEmptyString()
+		{
+			string[] strs = { "cat", "bcde", "cfde", "rat", "", "abcd", "acde", "bcfe", "cfda", "set" };
+			string result = LongestCommonPrefix.Calculate(strs);
+			Assert.Equal("", result);
+		}
+
+		[Fact]
+		public void MultipleStringsNoPrefix_ReturnEmptyString()
+		{
+			string[] strs = { "dog", "racecar", "car" };
+			string result = LongestCommonPrefix.Calculate(strs);
+			Assert.Equal("", result);
+		}
+
+		[Fact]
+		public void EqualStrings_ReturnPrefix()
+		{
+			string[] strs = { "abcde", "abcde", "abcde" };
+			string result = LongestCommonPrefix.Calculate(strs);
+			Assert.Equal("abcde", result);
+		}
+	}
+}
