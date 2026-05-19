a6e07391e9bbbf4d11d751542304c230ef645746
DzianCreator <dziancreator@gmail.com>
2026-05-20 01:45:53 +0200

    Add 'merge sorted lists'

---
+++ algorithms/Algorithms/MergeTwoSortedLists.cs
@@ -0,0 +1,63 @@
+﻿
+namespace Algorithms
+{
+	public class MergeTwoSortedLists
+	{
+		public static ListNode Calculate(ListNode list1, ListNode list2)
+		{
+			if (list1 == null)
+			{
+				return list2;
+			}
+			if (list2 == null)
+			{
+				return list1;
+			}
+			ListNode result = null;
+			ListNode current1 = list1;
+			ListNode current2 = list2;
+			if (list1.val < list2.val)
+			{
+				result = new ListNode(list1.val);
+				current1 = list1.next;
+			}
+			else
+			{
+				result = new ListNode(list2.val);
+				current2 = list2.next;
+			}
+			ListNode resultCurrent = result;
+			while (current1 != null || current2 != null)
+			{
+				if (current1 == null)
+				{
+					resultCurrent.next = new ListNode(current2.val);
+					current2 = current2.next;
+					resultCurrent = resultCurrent.next;
+				}
+				else if (current2 == null)
+				{
+					resultCurrent.next = new ListNode(current1.val);
+					current1 = current1.next;
+					resultCurrent = resultCurrent.next;
+				}
+				else
+				{
+					if (current1.val < current2.val)
+					{
+						resultCurrent.next = new ListNode(current1.val);
+						current1 = current1.next;
+						resultCurrent = resultCurrent.next;
+					}
+					else
+					{
+						resultCurrent.next = new ListNode(current2.val);
+						current2 = current2.next;
+						resultCurrent = resultCurrent.next;
+					}
+				}
+			}
+			return result;
+		}
+	}
+}
---
+++ algorithms/AlgorithmsTests/MergeTwoSortedListsTests.cs
@@ -0,0 +1,63 @@
+﻿using Algorithms;
+
+namespace AlgorithmsTests
+{
+	public class MergeTwoSortedListsTests
+	{
+		[Fact]
+		public void BothEmpty_ReturnEmpty()
+		{
+			var result = MergeTwoSortedLists.Calculate(null, null);
+			Assert.Null(result);
+		}
+
+		[Fact]
+		public void FirstEmpty_ReturnSecond()
+		{
+			var result = MergeTwoSortedLists.Calculate(null, new ListNode(1));
+			Assert.Equal(1, result.val);
+			Assert.Null(result.next);
+		}
+
+		[Fact]
+		public void SecondEmpty_ReturnFirst()
+		{
+			var result = MergeTwoSortedLists.Calculate(new ListNode(2), null);
+			Assert.Equal(2, result.val);
+			Assert.Null(result.next);
+		}
+
+		[Fact]
+		public void AllFirstAfterSecond_ReturnSorted()
+		{
+			var result = MergeTwoSortedLists.Calculate(new ListNode(3, new ListNode(4)), new ListNode(1, new ListNode(2)));
+			Assert.Equal(1, result.val);
+			Assert.Equal(2, result.next.val);
+			Assert.Equal(3, result.next.next.val);
+			Assert.Equal(4, result.next.next.next.val);
+			Assert.Null(result.next.next.next.next);
+		}
+
+		[Fact]
+		public void AllSecondAfterFirst_ReturnSorted()
+		{
+			var result = MergeTwoSortedLists.Calculate(new ListNode(1, new ListNode(2)), new ListNode(3, new ListNode(4)));
+			Assert.Equal(1, result.val);
+			Assert.Equal(2, result.next.val);
+			Assert.Equal(3, result.next.next.val);
+			Assert.Equal(4, result.next.next.next.val);
+			Assert.Null(result.next.next.next.next);
+		}
+
+		[Fact]
+		public void DifferentLists_ReturnSorted()
+		{
+			var result = MergeTwoSortedLists.Calculate(new ListNode(1, new ListNode(3)), new ListNode(2, new ListNode(4)));
+			Assert.Equal(1, result.val);
+			Assert.Equal(2, result.next.val);
+			Assert.Equal(3, result.next.next.val);
+			Assert.Equal(4, result.next.next.next.val);
+			Assert.Null(result.next.next.next.next);
+		}
+	}
+}
