2f714490f12330f9e4d55e1c6d386849bd82f5af
dziancreator <dziancreator@gmail.com>
2026-05-21 00:47:39 +0200

    Add 'generate parenthesis'

---
+++ algorithms/Algorithms/GenerateParenthesis.cs
@@ -0,0 +1,41 @@
+﻿using System.Text;
+
+namespace Algorithms
+{
+	public class GenerateParenthesis
+	{
+		public static IList<string> Generate(int n)
+		{
+			List<string> result = new List<string>();
+			if (n < 1)
+			{
+				return result;
+			}
+			Partial(result, new StringBuilder(), 0, 0, n);
+			return result;
+		}
+
+		public static void Partial(IList<string> list, StringBuilder stringBuilder, int open, int close, int max)
+		{
+			if (stringBuilder.Length == max + max)
+			{
+				list.Add(stringBuilder.ToString());
+			}
+			else
+			{
+				if (open < max)
+				{
+					stringBuilder.Append("(");
+					Partial(list, stringBuilder, open + 1, close, max);
+					stringBuilder.Length--;
+				}
+				if (close < max && close < open)
+				{
+					stringBuilder.Append(")");
+					Partial(list, stringBuilder, open, close + 1, max);
+					stringBuilder.Length--;
+				}
+			}
+		}
+	}
+}
---
+++ algorithms/AlgorithmsTests/GenerateParenthesisTests.cs
@@ -0,0 +1,43 @@
+﻿using Algorithms;
+
+namespace AlgorithmsTests
+{
+	public class GenerateParenthesisTests
+	{
+		[Fact]
+		public void Zero_ReturnEmptyArray()
+		{
+			var result = GenerateParenthesis.Generate(0);
+			Assert.Empty(result);
+		}
+
+		[Fact]
+		public void One_ReturnString()
+		{
+			var result = GenerateParenthesis.Generate(1);
+			Assert.Single(result);
+			Assert.Equal("()", result[0]);
+		}
+
+		[Fact]
+		public void Two_ReturnArray()
+		{
+			var result = GenerateParenthesis.Generate(2);
+			Assert.Equal(2, result.Count);
+			Assert.Contains("(())", result);
+			Assert.Contains("()()", result);
+		}
+
+		[Fact]
+		public void Three_ReturnArray()
+		{
+			var result = GenerateParenthesis.Generate(3);
+			Assert.Equal(5, result.Count);
+			Assert.Contains("((()))", result);
+			Assert.Contains("(()())", result);
+			Assert.Contains("(())()", result);
+			Assert.Contains("()(())", result);
+			Assert.Contains("()()()", result);
+		}
+	}
+}
